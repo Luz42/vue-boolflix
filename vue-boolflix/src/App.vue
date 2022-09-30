ba3091bfa7ef037564034c128d6d812b40a7254f
Luz42 <a.evangelista93@gmail.com>
2022-09-30 21:25:11 +0200

    MILESTONE 1 STEP 1
    creato un layout con cui effettuare una ricerca
    
    upgrade della funzione getApi
    
    viene aggiunta dinamicamente la ricerca del testo inserito in input
    
    il risultato della ricerca viene pushato in un Array e visualizzato
    
    NB: ripete l'operazione ad ogni evento scatenato, need to fix

--- vue-boolflix/src/App.vue
+++ vue-boolflix/src/App.vue
@@ -1,5 +1,19 @@
 <template>
   <div id="app">
+    <div>
+      <input @keyup.enter="showElements()" v-model="searchByName" type="text">
+      <button @click="getApi" >search</button>
+    </div>
+    <div class="row">
+      <div class="border col" v-for="movie in movies" :key="movie.id">
+        <ul>
+          <li>titolo: {{ movie.title }}</li>
+          <li>titolo originale: {{ movie.original_title }}</li>
+          <li>lingua originale: {{ movie.original_language }}</li>
+          <li>voto: {{ movie.vote_average }}</li>
+        </ul>
+    </div>
+    </div>
   </div>
 </template>
 
@@ -9,20 +23,36 @@ import { apiKey } from './env'
 
 export default {
   name: 'App',
+  data(){return{
+    searchByName:'',
+    movies:[],
+
+  }},
   components:{
 
   },
-  // mounted(){
-  //   this.getApi()
-  // },
+    // computed:{
+    //   showElements(){
+    //     this.getApi(this.searchByName)
+    //     return this.movies
+    //   }
+    // },
+
+  //all'avvio della ricerca la stringa che inserirò nell'API assumerà il valore contenuto(v-model)
   methods:{
+    //BISOGNA EVITARE DI FARE UNA NUOVA RICHIESTA SE IL VALORE RIMANE INVARIATO
+    //
     getApi(){
-      axios.get(`https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=ritorno+al+futuro`)
-      .then((response)=>{
-        console.log(response.data)
+      axios.get(`https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${this.searchByName}`)
+      .then(({data, status})=>{
+        if(status === 200){
+          this.movies = []
+          this.movies.push(...data.results)
+          console.log(this.movies)
+        }
       })
       .catch(error=> {
-        console.log(error)
+        console.log('Errore: ' + error.message)
       })
     }
   }
