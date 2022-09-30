596a0e766231e957411f6ebc373599f6b7604a94
Luz42 <a.evangelista93@gmail.com>
2022-09-30 17:27:55 +0200

    Sono stati installati Axios e Bootstrap
    importato bootstrap nell'scss di App

--- vue-boolflix/src/App.vue
+++ vue-boolflix/src/App.vue
@@ -5,6 +5,7 @@
 
 <script>
 
+
 export default {
   name: 'App',
   components:{
@@ -14,12 +15,14 @@ export default {
 </script>
 
 <style lang="scss">
+
+  @import './assets/main.scss';
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
