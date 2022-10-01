7f49c7af8c2f70c5b67cca54c092e0588589f567
Luz42 <a.evangelista93@gmail.com>
2022-10-01 14:11:46 +0200

    MILESTONE 2 STEP 1
    Installato e importato pack flag-icons

--- vue-boolflix/src/components/mainComponent.vue
+++ vue-boolflix/src/components/mainComponent.vue
@@ -1,11 +1,11 @@
 <template>
-  <main>
+  <main class="container">
     <div class="row">
       <div class="border col" v-for="movie in items" :key="movie.id">
         <ul>
           <li>titolo: {{ movie.title }}</li>
           <li>titolo originale: {{ movie.original_title }}</li>
-          <li>lingua originale: {{ movie.original_language }}</li>
+          <li>lingua originale: <span :class="`fi fi-${movie.original_language}`"></span> {{ movie.original_language }}</li>
           <li>voto: {{ movie.vote_average }}</li>
         </ul>
       </div>
@@ -14,12 +14,14 @@
 </template>
 
 <script>
+import "/node_modules/flag-icons/css/flag-icons.min.css";
+
 export default {
     name: 'mainComponent',
     props:{
         items: Array
+    },
     }
-}
 </script>
 
 <style scoped lang="scss">
