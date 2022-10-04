3eb65dfb3c5358113a91799deb705108e72cab5a
Luz42 <a.evangelista93@gmail.com>
2022-10-04 11:09:38 +0200

    fixed layout in mainComponent
    le sezioni collassavano tra loro

--- vue-boolflix/src/components/mainComponent.vue
+++ vue-boolflix/src/components/mainComponent.vue
@@ -1,12 +1,12 @@
 <template>
   <main class="container-fluid flex-grow-1 d-flex flex-column justify-content-center" style="overflow-y:auto">
-      <section style="height:50%" >
+      <section class="flex-grow-1" >
         <h4 class="p-3 px-4 m-0">MOVIES</h4>
         <div class="row px-5 pb-3 gap-4 d-flex flex-nowrap" style="overflow-x:auto">
           <moviesListComponent :movie="movie" v-for="movie in movies" :key="movie.id"/>
         </div>
       </section>
-      <section style="height:50%">
+      <section class="flex-grow-1">
         <h4 class="p-3 px-4 m-0">SERIES</h4>
         <div class="row px-5 pb-3 gap-4 d-flex flex-nowrap" style="overflow-x:auto">
           <seriesListComponent :serie="serie" v-for="serie in series" :key="serie.id"/>
